4c83c647b0b41eacb0c3cce6d59b408a36393386
iahub4metra <buhai.artem7@gmail.com>
2024-09-11 12:31:53 +0100

    done

---
+++ src/server.js
@@ -0,0 +1,61 @@
+import express from "express";
+import pino from 'pino-http';
+import cors from 'cors';
+import dotenv from "dotenv"
+import { getAllContacts, getContactById } from "./services/contacts.js";
+
+dotenv.config()
+
+const PORT = Number(process.env.PORT)
+
+export const setupServer = () => {
+    const app = express()
+
+    app.use(express.json());
+    app.use(cors());
+
+    app.use(
+        pino({
+        transport: {
+            target: 'pino-pretty',
+        },
+        }),
+    );
+
+    app.get('/contacts', async (reg, res) => {
+        const contacts = await getAllContacts();
+
+        res.status(200).json({
+            status: 200,
+            message: "Successfully found contacts!",
+            data: contacts,
+        })
+    });
+
+    app.get('/contacts/:contactId', async (reg, res) => {
+        const { contactId } = reg.params;
+        const contact = await getContactById(contactId)
+
+        if (!contact) {
+            res.status(404).json({
+                message:"Contact not found",
+            })
+        }
+
+        res.status(200).json({
+            status: 200,
+            message: `Successfully found contact with id ${contactId}`,
+            data: contact,
+        })
+    })
+
+    app.use('*', (reg, res) => {
+        res.status(404).json({
+            message:'Not found',
+        })
+    })
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
+}
